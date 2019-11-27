3c2157aa3e93673fedb82ae8c4f28889a798ad3e
fabricioMaxis <falus09@gmail.com>
2019-11-27 09:31:18 -0300

    ultimo dia de trabalho unibh

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const port = process.env.Port || 3000
 
 const convert = require('./lib/convert')
 const apiBCB = require('./lib/api.bcb')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/',async(req, res) => {
-    const cotacao =await apiBCB.getCotacao()
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao()
     res.render('home', {
         cotacao
     })
--- test-api.js
+++ test-api.js
@@ -1,6 +1,22 @@
-const axios = require('axios')
+/*const axios = require('axios')
 
 const url = ('https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2706-05-2019%27&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao')
 
 axios.get(url)
-.then(res => console.log(res.data.value[0].cotacaoVenda)) 
+.then(res => console.log(res.data.value[0].cotacaoVenda)) */
+
+
+const axios = require('axios')
+const getUrl = data => `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27${data}%27&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao`
+
+const getCotacaoAPI = (data) => axios.get(getUrl(data))
+const extracCotacao = res => res.data.value[0].cotacaoVenda
+const getToday = () =>{
+    const today = new Date()
+    return (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear()
+}
+console.log(today)
+
+{
+
+}
